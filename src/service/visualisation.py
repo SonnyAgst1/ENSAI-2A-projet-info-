975333781604e6eb6b935e45d70705fcbb9dfcb8
id2716-ensai <maelys.d-halluin@eleve.ensai.fr>
2025-11-04 09:49:29 +0000

    COUCHE DAO

---
+++ src/service/visualisation.py
@@ -0,0 +1,12 @@
+from typing import Dict, Optional
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
+import plotly.graph_objects as go
+import plotly.express as px
+from datetime import datetime, timedelta
+import calendar
+import numpy as np
+
+
+class VisualisationService:
